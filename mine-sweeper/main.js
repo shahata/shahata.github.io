57219fc76f16f2880d93c5f69de9b3b357713c6b
Shahar Talmi <shahart@wix.com>
2014-02-27 00:26:24 +0200

    added serialization

--- mine-sweeper/main.js
+++ mine-sweeper/main.js
@@ -1,59 +1,84 @@
-/* global document, alert, setTimeout */
+/* global document, alert, setTimeout, localStorage */
 'use strict';
 
-var mines = 6, width = 10, height = 8;
-var game, goodGuesses, badGuesses, initGame;
+var mines, width, height;
+var game, guesses, initGame, nodes;
+
+function saveGame() {
+  localStorage.minesweeper = JSON.stringify({options: {width: width, height: height, mines: mines}, game: game});
+}
+
+function loadGame() {
+  try {
+    var load = JSON.parse(localStorage.minesweeper);
+    if (load.game) {
+      document.getElementById('width').value = load.options.width;
+      document.getElementById('height').value = load.options.height;
+      document.getElementById('mines').value = load.options.mines;
+    }
+    return load.game;
+  } catch (e) {
+    return undefined;
+  }
+}
+
+function superman() {
+  var opacity = document.getElementById('superman').checked ? 0.5: 1;
+  for (var i = 0; i < nodes.length; i++) {
+    nodes[i].style.opacity = opacity;
+  }
+}
 
 function updateFlagsLeft() {
-  document.getElementById('flags-left').innerHTML = 'Flags left: ' + (mines - goodGuesses - badGuesses);
+  document.getElementById('flags-left').innerHTML = 'Flags left: ' + (mines - guesses.good - guesses.bad);
+}
+
+function invokeNeighbors(i, callback) {
+  if (i % width !== 0) {
+    callback(i - 1);
+    callback(i - width - 1);
+    callback(i + width - 1);
+  }
+  if (i % width !== width - 1) {
+    callback(i + 1);
+    callback(i - width + 1);
+    callback(i + width + 1);
+  }
+  callback(i - width);
+  callback(i + width);
 }
 
 function revealBlocks(i) {
-  if (i >= 0 && i < width * height) {
-    if (game[i].value === 0 && game[i].node.style.display !== 'none') {
-      var x = i;
-      game[i].node.style.display = 'none';
-      if (x % width !== 0) {
-        revealBlocks(x - 1);
-        revealBlocks(x - width - 1);
-        revealBlocks(x + width - 1);
-      }
-      if (x % width !== width - 1) {
-        revealBlocks(x + 1);
-        revealBlocks(x - width + 1);
-        revealBlocks(x + width + 1);
-      }
-      revealBlocks(x - width);
-      revealBlocks(x + width);
-    } else if (game[i].value !== '*') {
-      game[i].node.style.display = 'none';
+  if (i >= 0 && i < game.length && !game[i].revealed && !game[i].flagged) {
+    game[i].revealed = true;
+    nodes[i].style.display = 'none';
+
+    if (game[i].value === 0) {
+      invokeNeighbors(i, revealBlocks);
     }
   }
 }
 
 function handleClick(i, e) {
   if (e.shiftKey) {
-    if (goodGuesses + badGuesses === mines) {
+    if (guesses.good + guesses.bad === mines) {
       alert('No more flags left!!!');
     } else {
-      game[i].node.innerHTML = (game[i].node.innerHTML ? '' : 'Flag');
-      if (game[i].value === '*') {
-        goodGuesses += (game[i].node.innerHTML ? 1 : -1);
-      } else {
-        badGuesses += (game[i].node.innerHTML ? 1 : -1);
-      }
-      if (goodGuesses === mines) {
-        setTimeout(function () {
+      game[i].flagged = !game[i].flagged;
+      nodes[i].innerHTML = (game[i].flagged ? 'Flag' : '');
+      guesses[game[i].value === '*' ? 'good' : 'bad'] += (game[i].flagged ? 1 : -1);
+      if (guesses.good === mines) {
+        return setTimeout(function () {
           alert('You win!!!');
           initGame();
         }, 10);
       }
       updateFlagsLeft();
     }
-  } else {
+  } else if (!game[i].revealed && !game[i].flagged) {
     if (game[i].value === '*') {
-      game[i].node.style.display = 'none';
-      setTimeout(function () {
+      nodes[i].style.display = 'none';
+      return setTimeout(function () {
         alert('You lose!!!');
         initGame();
       }, 10);
@@ -61,56 +86,8 @@ function handleClick(i, e) {
       revealBlocks(i);
     }
   }
+  saveGame();
 }
-
-function updateCell(i) {
-  if (i >= 0 && i < width * height && game[i].value !== '*') {
-    game[i].value = game[i].value || 0;
-    game[i].value++;
-  }
-}
-
-initGame = function () {
-  game = [];
-  goodGuesses = 0;
-  badGuesses = 0;
-
-  for (var i = 0; i < width * height; i++) {
-    game[i] = {value: 0};
-  }
-
-  for (var generate = mines; generate > 0; generate--) {
-    var x = Math.floor(Math.random() * width * height);
-    if (game[x].value !== '*') {
-      game[x] = {value: '*'};
-      if (x % width !== 0) {
-        updateCell(x - 1);
-        updateCell(x - width - 1);
-        updateCell(x + width - 1);
-      }
-      if (x % width !== width - 1) {
-        updateCell(x + 1);
-        updateCell(x - width + 1);
-        updateCell(x + width + 1);
-      }
-      updateCell(x - width);
-      updateCell(x + width);
-    } else {
-      generate++;
-    }
-  }
-
-  var cells = document.getElementsByTagName('span');
-  for (i = 0; i < cells.length; i++) {
-    cells[i].innerHTML = game[i].value || '';
-    game[i].node = cells[i].parentNode.children[1];
-    game[i].node.style.display = 'block';
-    game[i].node.innerHTML = '';
-    cells[i].parentNode.onclick = handleClick.bind(undefined, i);
-  }
-
-  updateFlagsLeft();
-};
 
 function createRow() {
   var str = '';
@@ -128,5 +105,60 @@ function createBoard() {
   return str;
 }
 
-document.getElementById('game').innerHTML = createBoard();
-initGame();
+function newGame() {
+  var game = [];
+
+  function updateCell(i) {
+    if (i >= 0 && i < game.length && game[i].value !== '*') {
+      game[i].value++;
+    }
+  }
+
+  for (var i = 0; i < width * height; i++) {
+    game[i] = {value: 0, revealed: false, flagged: false};
+  }
+
+  for (i = 0; i < mines; i++) {
+    var x = Math.floor(Math.random() * game.length);
+    if (game[x].value !== '*') {
+      game[x].value = '*';
+      invokeNeighbors(x, updateCell);
+    } else {
+      i--;
+    }
+  }
+
+  return game;
+}
+
+initGame = function (loadedGame) {
+  document.getElementById('width').value = width = parseInt(document.getElementById('width').value) || 0;
+  document.getElementById('height').value = height = parseInt(document.getElementById('height').value) || 0;
+  document.getElementById('mines').value = mines = Math.min(width * height, parseInt(document.getElementById('mines').value) || 0);
+  game = loadedGame || newGame();
+  saveGame();
+
+  document.getElementById('game').innerHTML = createBoard();
+  var cells = document.getElementsByTagName('span');
+  guesses = {good: 0, bad: 0};
+  mines = 0;
+  nodes = [];
+
+  for (var i = 0; i < cells.length; i++) {
+    if (game[i].flagged) {
+      guesses[game[i].value === '*' ? 'good' : 'bad']++;
+    }
+    if (game[i].value === '*') {
+      mines++;
+    }
+    cells[i].innerHTML = game[i].value || '';
+    nodes[i] = cells[i].parentNode.children[1];
+    nodes[i].style.display = game[i].revealed ? 'none' : 'block';
+    nodes[i].innerHTML = game[i].flagged ? 'Flag' : '';
+    nodes[i].parentNode.onclick = handleClick.bind(undefined, i);
+  }
+  superman();
+  updateFlagsLeft();
+};
+
+initGame(loadGame());
